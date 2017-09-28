af75ed5d0c3e46513693eb5237325aca95597723
Michael Dippery <michael@monkey-robot.com>
2017-09-28 15:05:41 -0700

    Use Scala's Integral and Fractional classes instead of Jupiter's
    Scala has a numeric hierarchy like Haskell's, so use it instead of
    recreating our own.

--- src/main/scala/com/mipadi/jupiter/time/TemporalUnits.scala
+++ src/main/scala/com/mipadi/jupiter/time/TemporalUnits.scala
@@ -16,7 +16,7 @@
 
 package com.mipadi.jupiter.time
 
-import com.mipadi.jupiter.math.Numeric
+import scala.math.{Fractional, Integral}
 
 
 /** Convert longs, ints, and doubles to temporal units like seconds, minutes,
@@ -24,7 +24,7 @@ import com.mipadi.jupiter.math.Numeric
  */
 object TemporalUnits {
 
-  /** Implicitly converts a long, int, or double to temporal units like
+  /** Implicitly converts an integral value to temporal units like
    *  seconds, minutes, hours, etc.
    *
    *  With this conversion, callers can easily calculate temporal units from
@@ -36,15 +36,12 @@ object TemporalUnits {
    *  val minutes = seconds.minutes
    *  }}}
    *
-   *  @param wrapped
+   *  @param self
    *    The wrapped value
    *  @param ev
-   *    A divider for the given types of values. If one is not specified,
-   *    the proper divider from `[[com.mipadi.jupiter.math.Numeric
-   *    Numeric]]` is used, which should be appropriate for any int, long,
-   *    or double.
+   *    A divider for the given types of values
    */
-  implicit class Temporal[T](wrapped: T)(implicit ev: Numeric[T]) {
+  implicit class TemporalIntegral[T](self: T)(implicit ev: Integral[T]) {
     private val SecondsPerDay = 86400
     private val SecondsPerSiderealDay = 86164.0916
     private val DaysPerYear = 365
@@ -55,7 +52,7 @@ object TemporalUnits {
      *  @return
      *    The number of seconds represented by the wrapped value
      */
-    def seconds: T = wrapped
+    def seconds: T = self
 
     /** The number of minutes.
      *
@@ -64,7 +61,7 @@ object TemporalUnits {
      *  @return
      *    The number of minutes represented by the wrapped value
      */
-    def minutes: T = ev.divide(seconds, 60)
+    def minutes: T = ev.quot(seconds, ev.fromInt(60))
 
     /** The number of hours.
      *
@@ -73,7 +70,7 @@ object TemporalUnits {
      *  @return
      *    The number of hours represented by the wrapped value
      */
-    def hours: T = ev.divide(minutes, 60)
+    def hours: T = ev.quot(minutes, ev.fromInt(60))
 
     /** The number of calendar days.
      *
@@ -82,7 +79,7 @@ object TemporalUnits {
      *  @return
      *    The number of days represented by the wrapped value
      */
-    def days: T = ev.divide(seconds, SecondsPerDay)
+    def days: T = ev.quot(seconds, ev.fromInt(SecondsPerDay))
 
     /** The number of ''sidereal days''.
      *
@@ -93,7 +90,7 @@ object TemporalUnits {
      *  @return
      *    The number of sidereal days represented by the wrapped value
      */
-    def siderealDays: Double = ev.divide(seconds, SecondsPerSiderealDay)
+    def siderealDays: Double = ev.toDouble(seconds) / SecondsPerSiderealDay
 
     /** The number of weeks
      *
@@ -102,7 +99,7 @@ object TemporalUnits {
      *  @return
      *    The number of weeks represented by the wrapped value
      */
-    def weeks: T = ev.divide(days, 7)
+    def weeks: T = ev.quot(days, ev.fromInt(7))
 
     /** The number of years.
      *
@@ -111,7 +108,7 @@ object TemporalUnits {
      *  @return
      *    The number of years represented by the wrapped value
      */
-    def years: T = ev.divide(days, DaysPerYear)
+    def years: T = ev.quot(days, ev.fromInt(DaysPerYear))
 
     /** The number of ''astronomical years''.
      *
@@ -122,7 +119,104 @@ object TemporalUnits {
      *  @return
      *    The number of astronomical years represented by the wrapped value
      */
-    def astronomicalYears: Double =
-      ev.divide(seconds, SecondsPerAstronomicalYear.toDouble)
+    def astronomicalYears: Double = ev.toDouble(seconds) / SecondsPerAstronomicalYear.toDouble
+  }
+
+  /** Implicitly converts a fractional value to temporal units like
+   *  seconds, minutes, hours, etc.
+   *
+   *  With this conversion, callers can easily calculate temporal units from
+   *  a raw number:
+   *
+   *  {{{
+   *  import com.mipadi.jupiter.time.TemporalUnits._
+   *  val seconds = 864000
+   *  val minutes = seconds.minutes
+   *  }}}
+   *
+   *  @param self
+   *    The wrapped value
+   *  @param ev
+   *    A divider for the given types of values
+   */
+  implicit class TemporalFractional[T](self: T)(implicit ev: Fractional[T]) {
+    private val SecondsPerDay = 86400
+    private val SecondsPerSiderealDay = 86164.0916
+    private val DaysPerYear = 365
+    private val SecondsPerAstronomicalYear = 31557600
+
+    /** The number of seconds.
+     *
+     *  @return
+     *    The number of seconds represented by the wrapped value
+     */
+    def seconds: T = self
+
+    /** The number of minutes.
+     *
+     *  There are 60 seconds in a minute.
+     *
+     *  @return
+     *    The number of minutes represented by the wrapped value
+     */
+    def minutes: T = ev.div(seconds, ev.fromInt(60))
+
+    /** The number of hours.
+     *
+     *  There are 60 minutes or 3600 seconds in an hour.
+     *
+     *  @return
+     *    The number of hours represented by the wrapped value
+     */
+    def hours: T = ev.div(minutes, ev.fromInt(60))
+
+    /** The number of calendar days.
+     *
+     *  There are 24 hours or 86,400 seconds in an hour.
+     *
+     *  @return
+     *    The number of days represented by the wrapped value
+     */
+    def days: T = ev.div(seconds, ev.fromInt(SecondsPerDay))
+
+    /** The number of ''sidereal days''.
+     *
+     *  A ''sidereal day'' is the amount of time taken by Earth to rotate
+     *  on its axis relative to the stars. It is defined as 86164.0916
+     *  seconds, or about 4 minutes shorter than a solary day.
+     *
+     *  @return
+     *    The number of sidereal days represented by the wrapped value
+     */
+    def siderealDays: Double = ev.toDouble(seconds) / SecondsPerSiderealDay
+
+    /** The number of weeks
+     *
+     *  There are 7 days in a week.
+     *
+     *  @return
+     *    The number of weeks represented by the wrapped value
+     */
+    def weeks: T = ev.div(days, ev.fromInt(7))
+
+    /** The number of years.
+     *
+     *  Assumes a year of 365 days.
+     *
+     *  @return
+     *    The number of years represented by the wrapped value
+     */
+    def years: T = ev.div(days, ev.fromInt(DaysPerYear))
+
+    /** The number of ''astronomical years''.
+     *
+     *  An ''astronomical year'' is the amount of time it takes Earth to
+     *  revolve around the sun. It is equal to 31,557,600 seconds, or about
+     *  365.25 days.
+     *
+     *  @return
+     *    The number of astronomical years represented by the wrapped value
+     */
+    def astronomicalYears: Double = ev.toDouble(seconds) / SecondsPerAstronomicalYear.toDouble
   }
 }
